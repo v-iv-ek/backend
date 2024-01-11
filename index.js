c981770c1f3977c6d4fa611a6ab0d1ff1c8c8f77
v-iv-ek <91026668+v-iv-ek@users.noreply.github.com>
2024-01-11 16:31:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const env=require("dotenv").config()
 mongoDB();
 app.use(cors())
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-with,Content-TypeError,Accept"
